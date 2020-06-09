82503f92f965084bf271de3c0c12a306227b32dd
Razoli <murilo.s.razoli@gmail.com>
2020-06-09 14:21:29 -0300

    functions apply

---
+++ family_loops.R
@@ -0,0 +1,33 @@
+# Working with applicable families
+
+# You can use some function that can be useful in your project's like functions apply.
+
+quadratic_function <-function(x){x**2}
+print(quadratic_function(20)) # 400
+
+
+# apply () - arrays and matrix as arguments
+# tapply () - vectors can be divided into different subsets as arguments
+# lapply () - vectors and lists as arguments
+# sapply () - friendly version of lapply as arguments
+# vapply () - similar to sapply, with modified return value as arguments
+# rapply () - similar to lapply () as arguments
+# eapply () - generate a list as arguments
+# mapply () - similar to sapply, multivariate as arguments
+# by
+
+## sapply
+
+lista <- list(a = c(1:5), b = c(10:50)) # Return sum of each element from list
+sapply(lista, mean)
+sapply(lista, quadratic_function)
+
+## apply
+M <- matrix(c(1:10), nr = 5, nc = 5)
+apply(M, 1, sum) # Sum of lines
+apply(M, 2, sum) # Sum of columns
+
+## tapply
+#Calculates the FUN of values of each species, equals group by, FUN = SUM
+tapply(iris$Sepal.Length, iris$Species, sum)
+tapply(iris$Sepal.Length, iris$Species, mean)
