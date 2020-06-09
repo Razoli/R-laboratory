5c5de095a3f450a72385cd3b046347b9d4d0aecb
Razoli <murilo.s.razoli@gmail.com>
2020-06-09 17:16:05 -0300

    modifications

--- handling_large_data_fread.R
+++ handling_large_data_fread.R
@@ -1,12 +1,8 @@
 # How to read large data in R
-
 # Packages
-
 library("data.table")
 
 # You can use many functions that can be useful to read large datas > 400Mb in your memory.
-
 # Use fread to large datas - Fast and Friendly
 ?fread 
-
 system.time(df <- fread())
